fbb1f8072ec8ec06f66fd0694e739cbbab1f9a59
JLouback <juliana.m.louback@gmail.com>
2015-04-10 23:18:14 -0400

    q5 3 misaligned, 1 iteration, ~6 minutes runtime

--- question5.py
+++ question5.py
@@ -6,13 +6,14 @@ from question4 import init_t
 from question4 import model1
 import time
 import logging
+from itertools import izip
 
 """
 Usage:
 python question5.py corpus.en corpus.de > q5_output
 
 Question 5:  Initialize t(f|e) values with 5 iterations of IBM model 1
-(see question4.py) and q(j|i, l, m) with 1/l+1. Run 5 iters of IBM model 2
+(see question4.py) and q(j|i, l, m) with 1/l+1. Run 5 rounds of IBM model 2
 
 """
 
@@ -27,38 +28,41 @@ def init_params(source_corpus, foreign_corpus):
 
 # IBM model 2
 def model2(source_corpus, foreign_corpus, t):
-    source = file(source_corpus, "r")
-    foreign = file(foreign_corpus, "r")
     c_fe = defaultdict(int);c_e = defaultdict(int);c_ilm = defaultdict(int)
     c = defaultdict(int);q = defaultdict(int)
-    f_line = foreign.readline().strip()
-    e_line = source.readline().strip()
-    while e_line:
-        m = len(f_line.split(" "))
-        l = len(e_line.split(" "))
+    for e_line, f_line in izip(open(source_corpus), open(foreign_corpus)):
+        m = len(f_line.strip().split(" "))
+        l = len(e_line.strip().split(" "))
         e_line = "NULL " + e_line
         # Go over words in e, f
         for i in range(1, m+1):
-            f = f_line.split(" ")[i-1]
+            f = f_line.strip().split(" ")[i-1]
             # Calculate the delta denominator, set default values for  q(j|i,l,m)
             delta_d = 0
             for j in range(0, l+1):
-                e = e_line.split(" ")[j]
-                delta_d += q.get((j,i,l,m),1/float(l+1)) * t[e][f]
+                e = e_line.strip().split(" ")[j]
+                if (j,i,l,m) not in q:
+                    q[(j,i,l,m)] = 1/float(l+1)
+                delta_d += q[(j,i,l,m)] * t[e][f]
             for j in range(0, l+1):
-                e = e_line.split(" ")[j]
+                e = e_line.strip().split(" ")[j]
                 # Update rule
                 delta = (q[(j,i,l,m)] * t[e][f])/float(delta_d)
                 c_fe[(f,e)] += delta
                 c_e[e]+= delta
                 c_ilm[(i,l,m)] += delta
                 c[(j,i,l,m)] += delta
+    for e_line, f_line in izip(open(source_corpus), open(foreign_corpus)):
+        m = len(f_line.strip().split(" "))
+        l = len(e_line.strip().split(" "))
+        e_line = "NULL " + e_line
+        # Go over words in e, f
+        for i in range(1, m+1):
+            f = f_line.strip().split(" ")[i-1]
+            for j in range(0, l+1):
+                e = e_line.strip().split(" ")[j]
                 t[e][f] = float(c_fe[(f,e)]) / float(c_e[e])
                 q[(j,i,l,m)] = float(c[(j,i,l,m)]) / float(c_ilm[(i,l,m)])
-        f_line = foreign.readline().strip()
-        e_line = source.readline().strip()
-    source.close()
-    foreign.close()
     return t,q
 
 def main():
@@ -66,7 +70,7 @@ def main():
     #Read in corpus, initialize t(f|e) with 5 iterations of the EM algorithm for IBM model 1
     t = init_params(sys.argv[1],sys.argv[2])
     #Run  5 iterations of the EM algorithm for IBM model 1
-    for i in range(0,5):
+    for i in range(0,1):
         t,q = model2(sys.argv[1],sys.argv[2],t)
     #Find alignments for the first 20 sentence pairs in the training data
     source = file(sys.argv[1], "r")
@@ -94,6 +98,7 @@ def main():
         print(line)
         print(f_line)
         print(alignments)
+        print("")
 
     source.close()
     foreign.close()
